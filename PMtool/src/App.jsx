09dcaeaa16fabd677b459d6e31cdaf449b6f57e4
sachin <sharmasachinwins@gmail.com>
2024-08-14 15:06:54 +0530

    New project management task

---
+++ PMtool/src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+  return (
+    <main>
+      <Sidebar/>
+    </main>
+  );
+}
+
+export default App;
---
+++ PMtool/src/Components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <aside>
+        <h2>Your Project</h2>
+        <div>
+            <button>+Add Project</button>
+        </div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+        </ul>
+    </aside>
+  )
+}
